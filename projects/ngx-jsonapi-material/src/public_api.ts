95eefaa4361889bd1ee12a6aff9638f7ee3b85ce
max <max.ss@outlook.com>
2018-10-26 13:00:38 -0300

    floating filters component

--- projects/ngx-jsonapi-material/src/public_api.ts
+++ projects/ngx-jsonapi-material/src/public_api.ts
@@ -4,3 +4,6 @@
 
 export * from './lib/select/select.component';
 export * from './lib/select/select.module';
+
+export * from './lib/floating-filters/floating-filters.component';
+export * from './lib/floating-filters/floating-filters.module';
