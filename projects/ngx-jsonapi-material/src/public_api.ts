5b46846a767b8ee2f568dd220642eac8a18fdbed
max <max.ss@outlook.com>
2018-10-26 13:03:07 -0300

    AvoidDisabledStyleDirective componente migrado

--- projects/ngx-jsonapi-material/src/public_api.ts
+++ projects/ngx-jsonapi-material/src/public_api.ts
@@ -6,4 +6,5 @@ export * from './lib/select/select.component';
 export * from './lib/select/select.module';
 
 export * from './lib/floating-filters/floating-filters.component';
+export * from './lib/floating-filters/avoid-disabled-style.directive';
 export * from './lib/floating-filters/floating-filters.module';
