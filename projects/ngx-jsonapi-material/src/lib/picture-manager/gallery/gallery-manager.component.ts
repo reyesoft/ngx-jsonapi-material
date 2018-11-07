dda84e8bb2bd1a74fc7f6431c3f3fd04d11ebeae
maxSS <max.ss@outlook.com>
2018-11-07 07:20:10 -0300

    migrada jamGalleryManager a ngx-jsonapi.material

---
+++ projects/ngx-jsonapi-material/src/lib/picture-manager/gallery/gallery-manager.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { Resource } from 'ngx-jsonapi';
+
+@Component({
+    selector: 'jam-gallery-manager',
+    templateUrl: './gallery-manager.component.html',
+    styleUrls: ['./gallery-manager.component.scss']
+})
+export class GalleryManagerComponent implements OnInit {
+    @Input() public pictures: Array<Resource | any>;
+    @Input() public uploadUrl: string;
+    @Input() public limit: number;
+    @Input() public showDeleteOption: boolean = true;
+
+    @Output() public addPicture = new EventEmitter<string>();
+
+    public hide_if_have_limit: boolean;
+
+    public ngOnInit() {
+        // TODO: arreglar y descomentar en COL-1500
+        // this.hide_if_have_limit = this.validateIfHaveLimit();
+    }
+
+    public showPreview(img) {
+        this.addPicture.emit(img);
+    }
+
+    public validateIfHaveLimit() {
+        return this.limit ? this.pictures.length > this.limit : false;
+    }
+}
