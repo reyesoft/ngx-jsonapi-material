8f5bfd03e789fff4130a710724ad2dcb1e130e28
maxSS <max.ss@outlook.com>
2018-11-08 14:10:39 -0300

    jamPictureManager (#6)

--- projects/ngx-jsonapi-material/src/public_api.ts
+++ projects/ngx-jsonapi-material/src/public_api.ts
@@ -8,3 +8,7 @@ export * from './lib/select/select.module';
 export * from './lib/floating-filters/floating-filters.component';
 export * from './lib/floating-filters/avoid-disabled-style.directive';
 export * from './lib/floating-filters/floating-filters.module';
+
+export * from './lib/picture-manager/picture/picture-manager.component';
+export * from './lib/picture-manager/picture/image-change-interface';
+export * from './lib/picture-manager/picture-manager.module';
