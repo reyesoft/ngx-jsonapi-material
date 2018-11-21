e21b77e44a8937d4d87b72de2833f686b3d21427
MaxSaavedra_S <max.ss@outlook.com>
2018-11-21 00:56:08 -0300

    migrado rsDeleteConfirmation

---
+++ projects/ngx-jsonapi-material/src/lib/delete-confirmation/delete-confirmation.component.ts
@@ -0,0 +1,48 @@
+/***
+ * Copyright (C) 1997-2018 Reyesoft <info@reyesoft.com>
+ *
+ * This file is part of Multinexo. Multinexo can not be copied and/or
+ * distributed without the express permission of Reyesoft
+ */
+
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
+
+@Component({
+    selector: 'jam-delete-confirmation',
+    templateUrl: './delete-confirmation.component.html'
+})
+export class DeleteConfirmationComponent {
+    @Input() public icon: string;
+    @Input() public tooltip: string;
+    @Input() public msg: string;
+    @Input() public text: string;
+    @Input() public title: string;
+    @Input() public classed: string;
+    @Input() public styled: {};
+    @Input() public styleIcon: string;
+    @Input() public accept: string;
+    @Output() public deleteFc: EventEmitter<any> = new EventEmitter();
+
+    public constructor(
+        public dialog: MatDialog
+    ) {
+        this.styled = this.styled || { height: '45px' };
+        this.msg = this.msg || '¿Está seguro de eliminar?';
+        this.accept = this.accept || 'Sí';
+    }
+
+    public showConfirm(): void {
+        let dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+            width: 'auto',
+            data: { title: this.title, msg: this.msg, accept: this.accept }
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                this.deleteFc.emit();
+            }
+        });
+    }
+}
