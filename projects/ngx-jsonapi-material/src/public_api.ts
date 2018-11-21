a3293406ffa04c0a4ecca17454c1e3259d00a6cd
MaxSaavedra_S <max.ss@outlook.com>
2018-11-21 07:00:16 -0300

    migrado rsSubmit a jamSubmit

--- projects/ngx-jsonapi-material/src/public_api.ts
+++ projects/ngx-jsonapi-material/src/public_api.ts
@@ -5,6 +5,9 @@
 export * from './lib/select/select.component';
 export * from './lib/select/select.module';
 
+export * from './lib/submit/submit.component';
+export * from './lib/submit/select.module';
+
 export * from './lib/floating-filters/floating-filters.component';
 export * from './lib/floating-filters/avoid-disabled-style.directive';
 export * from './lib/floating-filters/floating-filters.module';
