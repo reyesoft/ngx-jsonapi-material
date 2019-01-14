049df3a29628ed10b6ad27f84455762f1b01fc55
maxSS <max.ss@outlook.com>
2019-01-14 18:13:33 -0300

    Jam 1873 implementar jam menu (#25)
    * nuevas caracteristicas soportadas en jamMenu
    
    * cambios arreglados
    
    * actualizacion de condiciones disabled y hidden
    
    * actualizacion de version

--- projects/ngx-jsonapi-material/src/lib/menu/button.ts
+++ projects/ngx-jsonapi-material/src/lib/menu/button.ts
@@ -1,9 +1,11 @@
 export class Button {
     public id = 'string';
-    public attributes = {
+    public attributes: Attributes = {
         icon: '',
         label: '',
-        class: ''
+        class: '',
+        disabled: false,
+        hidden: false
     }
 
     public constructor(id: string) {
@@ -16,14 +18,25 @@ export class Button {
         return this;
     }
 
-    public addButtonAttributes(attributes: any) {
+    public addButtonAttributes(attributes: Attributes) {
         this.attributes = { ...this.attributes, ...attributes };
 
         return this;
     }
 }
 
+export interface Attributes {
+    label: string;
+    icon?: string;
+    class?: string;
+    hidden?: boolean;
+    disabled?: boolean;
+    custom_icon_svg?: string;
+    custom_icon_font?: string;
+}
+
 export interface Option {
     section?: string;
+    hidden?: boolean;
     buttons: Array<Button>;
 }
--- projects/ngx-jsonapi-material/src/lib/menu/menu.component.ts
+++ projects/ngx-jsonapi-material/src/lib/menu/menu.component.ts
@@ -1,24 +1,38 @@
-import { Component, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnDestroy, OnChanges } from '@angular/core';
 import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
 import { MatBottomSheet } from '@angular/material';
 import { filter } from 'rxjs/operators';
 import { Destroyer } from '../destroyer';
-import { Option } from './button';
+import { Option, Button } from './button';
 
 @Component({
     selector: 'jam-menu',
     styleUrls: ['./menu.component.scss'],
     templateUrl: './menu.component.html'
 })
-export class MenuComponent implements OnDestroy {
+export class MenuComponent implements OnDestroy, OnChanges {
     @Input() public options: Array<Option>;
-    @Output() public selected = new EventEmitter<string>();
+    @Input() public disabledButtons: Array<string>;
+    @Input() public hiddenButtons: Array<string>;
+    @Input() public hiddenSections: Array<string>;
+    @Input() public data: Array<any>;
+    @Output() public selected = new EventEmitter<{ key: string, data?: Array<any> }>();
 
     public destroyer = new Destroyer();
 
     public constructor(
         private matBottomSheet : MatBottomSheet
     ) { }
+
+    public ngOnChanges(changes) {
+        if (changes.disabledButtons) {
+            this.updateButtonAttributes('disabled', true, this.disabledButtons);
+        }
+
+        if (changes.hiddenButtons) {
+            this.updateButtonAttributes('hidden', true, this.hiddenButtons);
+        }
+    }
 
     public ngOnDestroy(): void {
         this.destroyer.destroy();
@@ -33,10 +47,47 @@ export class MenuComponent implements OnDestroy {
             this.destroyer.pipe(),
             filter(response => ![null, undefined, ''].includes(response))
         )
-        .subscribe(response => this.selected.emit(response));
+        .subscribe(response => this.selected.emit(this.getEmit(response)));
     }
 
     public selectedOption(selected: string): void {
-        this.selected.emit(selected);
+        this.selected.emit(this.getEmit(selected));
+    }
+
+    private getEmit(response: string) {
+        return { key: response, data: this.data || null };
+    }
+
+    private updateButtonAttributes(attribute: string, value: any, button_ids: Array<string>) {
+        for (let button_id of button_ids) {
+            for (let option of this.options) {
+                let button = this.searchButton(button_id, option);
+                if (!button) continue;
+                button.attributes[attribute] = value;
+            }
+        }
+
+        this.hiddenSection();
+    }
+
+    private searchButton(button_id: string, option: Option): Button {
+        return option.buttons.find((button: Button) => {
+            return button.id === button_id;
+        });
+    }
+
+    private hiddenSection(): void {
+        for (let option of this.options) {
+            let count = 0;
+            for (let button of option.buttons) {
+                if (button.attributes.hidden) {
+                    count++
+                }
+
+                if (option.buttons.length === count) {
+                    option.hidden = true;
+                }
+            }
+        }
     }
 }
