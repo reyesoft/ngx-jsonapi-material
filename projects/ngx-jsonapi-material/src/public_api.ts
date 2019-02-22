66cd0284e2d7ff2f4376dc93f06c4a1d234259af
max <max.ss@outlook.com>
2019-02-22 11:36:37 -0300

    directive que controla el estado de los expansion panel de material

--- projects/ngx-jsonapi-material/src/public_api.ts
+++ projects/ngx-jsonapi-material/src/public_api.ts
@@ -54,3 +54,6 @@ export * from './lib/dynamic-forms/dynamic-forms.module';
 
 export * from './lib/tabs/tabs.directive';
 export * from './lib/tabs/tabs.module';
+
+export * from './lib/expansion-panel/remember-state.directive';
+export * from './lib/expansion-panel/remember-state.module';
