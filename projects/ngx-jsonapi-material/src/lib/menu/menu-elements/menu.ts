425b1b5cc9a35ae226301a9724fd08e8f662b05d
Maxi <maxi7587@hotmail.com>
2019-03-18 12:40:53 -0300

    Fix menu sections and buttons (#34)
    * fixed svg buttons in jam menu
    * added main image option to jam menu

--- projects/ngx-jsonapi-material/src/lib/menu/menu-elements/menu.ts
+++ projects/ngx-jsonapi-material/src/lib/menu/menu-elements/menu.ts
@@ -4,6 +4,7 @@ import { MenuElementsCollection, MenuElement } from './menu-elements';
 
 export class Menu extends MenuElementsCollection<Section> {
     public data: Array<Section> = <Array<Section>>[];
+    public main_image: {url: string; styles?: {[key: string]: string}};
     public findSection: (arg: string) => Section = <(arg: string) => Section>this.find;
     public addSections = this.add;
     public removeEmptySections() {
@@ -14,5 +15,10 @@ export class Menu extends MenuElementsCollection<Section> {
         }
 
         return false;
+    }
+    public setMainImage(image_data: {url: string; styles?: {[key: string]: string}}): Menu {
+        this.main_image = image_data;
+
+        return this;
     }
 }
