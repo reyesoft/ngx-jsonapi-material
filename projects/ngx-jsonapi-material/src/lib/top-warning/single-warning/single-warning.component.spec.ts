8839cf337880e81e2ff34fde9b04c05efa76d326
max <max.ss@outlook.com>
2019-11-15 17:00:33 -0300

    single warning

---
+++ projects/ngx-jsonapi-material/src/lib/top-warning/single-warning/single-warning.component.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { SingleWarningComponent } from '../single-warning/single-warning.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+
+describe('NgxJsonapiMaterialComponent', () => {
+    let component: SingleWarningComponent;
+    let fixture: ComponentFixture<SingleWarningComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            schemas: [NO_ERRORS_SCHEMA],
+            declarations: [ SingleWarningComponent ],
+            imports: [NoopAnimationsModule]
+        })
+        .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SingleWarningComponent);
+        component = fixture.componentInstance;
+        // component.collection = new DocumentCollection();
+        // component.toRelate = new Resource();
+        fixture.detectChanges();
+    });
+
+    // it('if message is defined, then the component should be shown.', () => {
+    //     component.message = 'Esto es un mensaje';
+    //     expect(component.message).toBeDefined();
+    //     fixture.detectChanges();
+    //     let single_warning_component = fixture.debugElement.query(By.css('mat-card'));
+    //     expect(single_warning_component).toBeDefined();
+    // });
+
+    it('if message is not defined, then the component should not be shown.', () => {
+        // expect(component.message).toBeUndefined();
+        // fixture.detectChanges();
+        let single_warning_component = fixture.debugElement.query(By.css('mat-card'));
+        expect(single_warning_component).toBeUndefined();
+    });
+});
