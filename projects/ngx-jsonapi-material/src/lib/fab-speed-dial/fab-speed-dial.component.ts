e98360422a369473d5b09824d621bcb1236c00c8
maxi7587 <maxi7587@hotmail.com>
2019-12-06 11:34:08 -0300

    added-fab-buttons

---
+++ projects/ngx-jsonapi-material/src/lib/fab-speed-dial/fab-speed-dial.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FabSpeedDialMiniButton } from './fab-speed-dial-mini-button';
+
+@Component({
+    selector: 'jam-fab-speed-dial',
+    templateUrl: './fab-speed-dial.component.html',
+    styleUrls: ['./fab-speed-dial.component.scss']
+})
+export class FabSpeedDialComponent implements OnInit {
+
+    @Input() public animationMode: string = 'scale';
+    @Input() public tooltip: string = '';
+    @Input() public spin: boolean = true;
+    @Input() public icon: string = 'add';
+    @Input() public routerLink: Array<string> = [];
+    @Input() public queryParams: {[key: string]: any};
+    @Input() public fabSpeedDialMiniButtons: Array<FabSpeedDialMiniButton> = [];
+
+    @Output() public fabSpeedDialClick: EventEmitter<void> = new EventEmitter();
+    @Output() public actionsClick: EventEmitter<string> = new EventEmitter();
+
+    public fab_status = {
+        opened: false,
+        status: 'closed'
+    };
+
+    public constructor(private activatedRoute: ActivatedRoute) {}
+
+    public ngOnInit() {
+        if (!this.queryParams) {
+            this.queryParams = this.activatedRoute.snapshot.queryParams;
+        }
+    }
+
+    public toggleFabStatus(status) {
+        if (status === 'open') {
+            this.fab_status.status = 'opened';
+            this.fab_status.opened = true;
+        } else {
+            this.fab_status.status = 'closed';
+            setTimeout(() => {
+                if (this.fab_status.status === 'closed') {
+                    this.fab_status.opened = false;
+                }
+            }, 300);
+        }
+    }
+
+}
